db1ade1b49114945d58cfb591cceb5c69ed8923d
CoolShishir <jainshishir18@gmail.com>
2018-06-13 18:33:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,8 +26,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
 
-// https://accounts.google.com
-// /o/oauth2/v2/auth?
-// response_type=code&
-// redirect_uri=http%3A%2F%2F
-// localhost%3A5000%2Fauth%2Fauth%2Fgoogle%2Fcallback&scope=profile%20email&client_id=202556203199-1svr4427p45r5id086kmvscb2s6jg0q7.apps.googleusercontent.com
+
